d9bbc7db1ca10ed1fb03e526ccf7ce359929ef91
salvanicola <nicola.salvadore@gmail.com>
2022-09-07 17:12:52 +0200

    Fix data and template typo

--- Kerbero.NET/src/WebApi/ConfigureServices.cs
+++ Kerbero.NET/src/WebApi/ConfigureServices.cs
@@ -2,7 +2,7 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ConfigureServices
 {
-    public static IServiceCollection AddWebUIServices(this IServiceCollection services)
+    public static IServiceCollection AddWebApiServices(this IServiceCollection services)
     {
         return services;
     }
--- Kerbero.NET/src/WebApi/Program.cs
+++ Kerbero.NET/src/WebApi/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
-builder.Services.AddWebUIServices();
+builder.Services.AddWebApiServices();
 
 
 builder.Services.AddControllers();
