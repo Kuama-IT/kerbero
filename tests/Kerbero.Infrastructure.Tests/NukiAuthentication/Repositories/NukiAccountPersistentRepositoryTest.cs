a70cb01c931bb9ecf98a6c9ce58286482de86365
Nicola <nicola.salvadore@gmail.com>
2022-10-19 17:55:05 +0200

    fix: test for db context

--- tests/Kerbero.Infrastructure.Tests/NukiAuthentication/Repositories/NukiAccountPersistentRepositoryTest.cs
+++ tests/Kerbero.Infrastructure.Tests/NukiAuthentication/Repositories/NukiAccountPersistentRepositoryTest.cs
@@ -2,31 +2,32 @@ using FluentAssertions;
 using Kerbero.Domain.Common.Errors;
 using Kerbero.Domain.NukiAuthentication.Entities;
 using Kerbero.Domain.NukiAuthentication.Repositories;
-using Kerbero.Infrastructure.Common.Interfaces;
+using Kerbero.Infrastructure.Common.Context;
 using Kerbero.Infrastructure.NukiAuthentication.Repositories;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.Extensions.Logging;
 using Moq;
-using Npgsql;
 
 namespace Kerbero.Infrastructure.Tests.NukiAuthentication.Repositories;
 
-public class NukiAccountPersistentRepositoryTest
+public class NukiAccountPersistentRepositoryTest: IDisposable
 {
 	private readonly NukiAccountPersistentRepository _repository;
-	private readonly Mock<IApplicationDbContext> _dbContext;
-	private readonly Mock<DbSet<NukiAccount>> _dbSetNukiAccount;
+	private readonly ApplicationDbContext _dbContext;
 
 	public NukiAccountPersistentRepositoryTest()
 	{
+		var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+			.UseInMemoryDatabase(databaseName: "AppDbContext")
+			.Options;
 		var logger = new Mock<ILogger<NukiAccountPersistentRepository>>();
-		_dbSetNukiAccount = new Mock<DbSet<NukiAccount>>();
-		_dbContext = new Mock<IApplicationDbContext>();
-		_dbContext.Setup(c => c.NukiAccounts).Returns(_dbSetNukiAccount.Object);
-		_repository = new NukiAccountPersistentRepository(_dbContext.Object, logger.Object);
+		_dbContext = new ApplicationDbContext(options);
+		_repository = new NukiAccountPersistentRepository(_dbContext, logger.Object);
+	}
+	
+	public void Dispose()
+	{
+		_dbContext.Dispose();
 	}
 
 	#region CreateNukiAccount
@@ -54,80 +55,14 @@ public class NukiAccountPersistentRepositoryTest
 			ClientId = nukiAccount.ClientId,
 			TokenType = nukiAccount.TokenType,
 		};
-		
-		// Workaround to mock an EntityEntry, MS not provides a way to test a context with EF 6+
-		var internalEntityEntry = new InternalEntityEntry(
-			new Mock<IStateManager>().Object,
-			new RuntimeEntityType("NukiAccount", typeof(NukiAccount), false, null!, null, null, ChangeTrackingStrategy.Snapshot, null, false),
-			data);
 
-		var entityEntry = new Mock<EntityEntry<NukiAccount>>(internalEntityEntry);
-		_dbSetNukiAccount.Setup(c => c.Add(It.IsAny<NukiAccount>()))
-			.Returns(entityEntry.Object);
-		entityEntry.Setup(c => c.Entity).Returns(data);
-
-		
 		// Act
 		var res = await _repository.Create(nukiAccount);
 		
 		// Assert
 		nukiAccount.Id = 1;
 		res.Value.Should().BeEquivalentTo(nukiAccount);
-		res.Value.Should().NotBeSameAs(nukiAccount);
-		_dbSetNukiAccount.Verify(c => c.Add(It.Is<NukiAccount>(e => e.Equals(nukiAccount))));
-		_dbContext.Verify(c => c.SaveChangesAsync(CancellationToken.None));
 	}
-
-	[Fact]
-	public async Task CreateNukiAccount_PersistentResourceNotAvailable_Test()
-	{
-		// Arrange
-		var nukiAccount = new NukiAccount
-		{
-			Token = "VALID_TOKEN",
-			RefreshToken = "VALID_REFRESH_TOKEN",
-			TokenExpiringTimeInSeconds = 2592000,
-			ClientId = "VALID_CLIENT_ID",
-			TokenType = "bearer",
-		};
-
-		_dbSetNukiAccount.Setup(c => c.Add(It.IsAny<NukiAccount>()))
-			.Throws(new NotSupportedException());
-
-		// Act
-		var res = await _repository.Create(nukiAccount);
-
-		// Assert
-		res.IsFailed.Should().BeTrue();
-		res.Errors.First().Should().BeEquivalentTo(new PersistentResourceNotAvailableError());
-	}
-	
-	[Fact]
-	public async Task CreateNukiAccount_DuplicateEntry_Test()
-	{
-		// Arrange
-		var nukiAccount = new NukiAccount
-		{
-			Token = "VALID_TOKEN",
-			RefreshToken = "VALID_REFRESH_TOKEN",
-			TokenExpiringTimeInSeconds = 2592000,
-			ClientId = "VALID_CLIENT_ID",
-			TokenType = "bearer",
-		};
-
-		int.TryParse(PostgresErrorCodes.UniqueViolation, out var errCode);
-		_dbSetNukiAccount.Setup(c => c.Add(It.IsAny<NukiAccount>()))
-			.Throws(new DbUpdateException("something", new DbUpdateException("something", new NpgsqlException{HResult = errCode})));
-
-		// Act
-		var res = await _repository.Create(nukiAccount);
-
-		// Assert
-		res.IsFailed.Should().BeTrue();
-		res.Errors.First().Should().BeEquivalentTo(new DuplicateEntryError("Nuki account"));
-		res.Errors.First().Message.Should().Contain("The Nuki account already exists, try to update instead.");
-	}
-	
 	#endregion
 
 	#region GetNukiAccount
@@ -136,33 +71,23 @@ public class NukiAccountPersistentRepositoryTest
 	public void GetNukiAccount_Success_Test()
 	{
 		// Arrange
-		var dataList = new List<NukiAccount>();
-		for (var i = 0; i < 3; i++)
+		for (var i = 1; i < 3; i++)
 		{
 			var nukiAccount = new NukiAccount
 			{
-				Id = i,
-				Token = "VALID_TOKEN",
+				Token = "VALID_TOKEN" + i,
 				RefreshToken = "VALID_REFRESH_TOKEN",
 				TokenExpiringTimeInSeconds = 2592000,
-				ClientId = "VALID_CLIENT_ID",
+				ClientId = "VALID_CLIENT_ID" + i,
 				TokenType = "bearer",
 			};
-			dataList.Add(nukiAccount);
+			_dbContext.NukiAccounts.Add(nukiAccount);
 		}
-		
-		var data = dataList.AsQueryable();
 
-		var mockSet = new Mock<DbSet<NukiAccount>>();
-		mockSet.As<IQueryable<NukiAccount>>().Setup(m => m.Provider).Returns(data.Provider);
-		mockSet.As<IQueryable<NukiAccount>>().Setup(m => m.Expression).Returns(data.Expression);
-		mockSet.As<IQueryable<NukiAccount>>().Setup(m => m.ElementType).Returns(data.ElementType);
-		mockSet.As<IQueryable<NukiAccount>>().Setup(m => m.GetEnumerator()).Returns(() => data.GetEnumerator());
+		_dbContext.SaveChanges();
 		
 		_repository.Should().BeAssignableTo<INukiAccountPersistentRepository>();
 
-		_dbContext.Setup(c => c.NukiAccounts).Returns(mockSet.Object);
-		
 		// Act
 		var res = _repository.GetAccount(1);
 		
@@ -170,10 +95,10 @@ public class NukiAccountPersistentRepositoryTest
 		res.Value.Should().BeEquivalentTo(new NukiAccount
 		{
 			Id = 1,
-			Token = "VALID_TOKEN",
+			Token = "VALID_TOKEN1",
 			RefreshToken = "VALID_REFRESH_TOKEN",
 			TokenExpiringTimeInSeconds = 2592000,
-			ClientId = "VALID_CLIENT_ID",
+			ClientId = "VALID_CLIENT_ID1",
 			TokenType = "bearer",
 		});
 	}
@@ -182,35 +107,9 @@ public class NukiAccountPersistentRepositoryTest
 	public void GetNukiAccount_NotValidProviderAccountFoundError_Test()
 	{
 		// Arrange
-		var dataList = new List<NukiAccount>();
-		for (var i = 2; i < 5; i++)
-		{
-			var nukiAccount = new NukiAccount
-			{
-				Id = i,
-				Token = "VALID_TOKEN",
-				RefreshToken = "VALID_REFRESH_TOKEN",
-				TokenExpiringTimeInSeconds = 2592000,
-				ClientId = "VALID_CLIENT_ID",
-				TokenType = "bearer",
-			};
-			dataList.Add(nukiAccount);
-		}
-		
-		var data = dataList.AsQueryable();
 
-		var mockSet = new Mock<DbSet<NukiAccount>>();
-		mockSet.As<IQueryable<NukiAccount>>().Setup(m => m.Provider).Returns(data.Provider);
-		mockSet.As<IQueryable<NukiAccount>>().Setup(m => m.Expression).Returns(data.Expression);
-		mockSet.As<IQueryable<NukiAccount>>().Setup(m => m.ElementType).Returns(data.ElementType);
-		mockSet.As<IQueryable<NukiAccount>>().Setup(m => m.GetEnumerator()).Returns(() => data.GetEnumerator());
-		
-		_repository.Should().BeAssignableTo<INukiAccountPersistentRepository>();
-
-		_dbContext.Setup(c => c.NukiAccounts).Returns(mockSet.Object);
-		
 		// Act
-		var res = _repository.GetAccount(1);
+		var res = _repository.GetAccount(0);
 	
 		// Assert
 		res.IsFailed.Should().BeTrue();
@@ -234,89 +133,16 @@ public class NukiAccountPersistentRepositoryTest
 			ClientId = "VALID_CLIENT_ID",
 			TokenType = "bearer",
 		};
-		
-		var data = new NukiAccount
-		{
-			Id = 1,
-			Token = nukiAccount.Token,
-			RefreshToken = nukiAccount.RefreshToken,
-			TokenExpiringTimeInSeconds = nukiAccount.TokenExpiringTimeInSeconds,
-			ClientId = nukiAccount.ClientId,
-			TokenType = nukiAccount.TokenType,
-		};
-		
-		// Workaround to mock an EntityEntry, MS not provides a way to test a context with EF 6+
-		var internalEntityEntry = new InternalEntityEntry(
-			new Mock<IStateManager>().Object,
-			new RuntimeEntityType("NukiAccount", typeof(NukiAccount), false, null!, null, null, ChangeTrackingStrategy.Snapshot, null, false),
-			data);
 
-		var entityEntry = new Mock<EntityEntry<NukiAccount>>(internalEntityEntry);
-		_dbSetNukiAccount.Setup(c => c.Update(It.IsAny<NukiAccount>()))
-			.Returns(entityEntry.Object);
-		entityEntry.Setup(c => c.Entity).Returns(data);
-
-		
 		// Act
 		var res = await _repository.Update(nukiAccount);
 		
 		// Assert
 		nukiAccount.Id = 1;
 		res.Value.Should().BeEquivalentTo(nukiAccount);
-		res.Value.Should().NotBeSameAs(nukiAccount);
-		_dbSetNukiAccount.Verify(c => c.Update(It.Is<NukiAccount>(e => e.Equals(nukiAccount))));
-		_dbContext.Verify(c => c.SaveChangesAsync(CancellationToken.None));
 	}
 
-	[Fact]
-	public async Task Update_PersistentResourceNotAvailable_Test()
-	{
-		// Arrange
-		var nukiAccount = new NukiAccount
-		{
-			Token = "VALID_TOKEN",
-			RefreshToken = "VALID_REFRESH_TOKEN",
-			TokenExpiringTimeInSeconds = 2592000,
-			ClientId = "VALID_CLIENT_ID",
-			TokenType = "bearer",
-		};
-
-		_dbSetNukiAccount.Setup(c => c.Update(It.IsAny<NukiAccount>()))
-			.Throws(new NotSupportedException());
-
-		// Act
-		var res = await _repository.Update(nukiAccount);
-
-		// Assert
-		res.IsFailed.Should().BeTrue();
-		res.Errors.First().Should().BeEquivalentTo(new PersistentResourceNotAvailableError());
-	}
-	
-	[Fact]
-	public async Task UpdateNukiAccount_DuplicateEntry_Test()
-	{
-		// Arrange
-		var nukiAccount = new NukiAccount
-		{
-			Token = "VALID_TOKEN",
-			RefreshToken = "VALID_REFRESH_TOKEN",
-			TokenExpiringTimeInSeconds = 2592000,
-			ClientId = "VALID_CLIENT_ID",
-			TokenType = "bearer",
-		};
-
-		int.TryParse(PostgresErrorCodes.UniqueViolation, out var errCode);
-		_dbSetNukiAccount.Setup(c => c.Update(It.IsAny<NukiAccount>()))
-			.Throws(new DbUpdateException("something", new DbUpdateException("something", new NpgsqlException{HResult = errCode})));
-
-		// Act
-		var res = await _repository.Update(nukiAccount);
-
-		// Assert
-		res.IsFailed.Should().BeTrue();
-		res.Errors.First().Should().BeEquivalentTo(new DuplicateEntryError("Nuki account"));
-		res.Errors.First().Message.Should().Contain("The Nuki account already exists, try to update instead.");
-	}
-	
 	#endregion
+
+
 }
