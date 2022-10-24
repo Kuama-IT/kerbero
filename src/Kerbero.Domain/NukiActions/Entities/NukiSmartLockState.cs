f0748e29a50cdf0959068a9a05fb625debe0f895
salvanicola <nicola.salvadore@gmail.com>
2022-10-24 17:03:05 +0200

    feat: interactor to create smartlock

---
+++ src/Kerbero.Domain/NukiActions/Entities/NukiSmartLockState.cs
@@ -0,0 +1,22 @@
+namespace Kerbero.Domain.NukiActions.Entities;
+
+public class NukiSmartLockState
+{
+    public int Mode { get; set; }
+		
+    public int State { get; set; }
+		
+    public int LastAction { get; set; }
+		
+    public bool BatteryCritical { get; set; }
+		
+    public bool BatteryCharging { get; set; }
+		
+    public int BatteryCharge { get; set; }
+		
+    public int DoorState { get; set; }
+		
+    public string? OperationId { get; set; }
+
+    public NukiSmartLock NukiSmartLock { get; set; } = null!;
+}
