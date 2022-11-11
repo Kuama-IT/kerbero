fae7c6c731d3dd7d98d7a210e37410ea1bc718f6
salvanicola <nicola.salvadore@gmail.com>
2022-11-11 11:49:56 +0100

    feat: added scripts and descriptions for deploy

---
+++ kerbero-setup.sh
@@ -0,0 +1,18 @@
+# DB set up
+echo '\033[4;32mDB compose\033[0m'
+cd database
+docker compose up -d
+cd ..
+
+# .NET module setup
+echo '\033[4;32m.NET restore packages\033[0m'
+cd web-api
+# dotnet ef database update -s src/Kerbero.WebApi/Kerbero.WebApi.csproj -p src/Kerbero.Infrastructure/Kerbero.Infrastructure.csproj
+dotnet restore
+cd ..
+
+# Vue module setup
+echo '\033[4;32mVue module setup\033[0m'
+cd web-app
+npm install
+cd ..
