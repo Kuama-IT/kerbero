539e0b485ebb0e244a688962c465372fd9327662
Daniele De Matteo <daniele@kuama.net>
2022-12-01 13:59:45 +0100

    feat: make nuki credentials refreshable

--- web-api/src/Kerbero.Infrastructure/NukiCredentials/Repositories/NukiCredentialRepository.cs
+++ web-api/src/Kerbero.Infrastructure/NukiCredentials/Repositories/NukiCredentialRepository.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using FluentResults;
 using Flurl;
 using Flurl.Http;
@@ -80,9 +81,50 @@ public class NukiCredentialRepository : INukiCredentialRepository
     }
   }
 
-  public Task<Result<NukiCredentialModel>> CreateDraft(NukiCredentialDraftModel model)
+  public async Task<Result<NukiCredentialModel>> CreateDraft(NukiCredentialDraftModel model)
   {
-    throw new NotImplementedException();
+    try
+    {
+      var entity = NukiCredentialMapper.Map(model);
+
+      _dbContext.NukiCredentials.Add(entity);
+
+      var pivotEntity = new UserNukiCredentialEntity
+      {
+        NukiCredential = entity,
+        UserId = model.UserId,
+      };
+
+      _dbContext.UserNukiCredentials.Add(pivotEntity);
+
+      await _dbContext.SaveChangesAsync();
+
+      return NukiCredentialMapper.Map(entity);
+    }
+    catch (NotSupportedException exception)
+    {
+      _logger.LogError(exception, "Error while adding a NukiCredential (draft) to the database");
+      return Result.Fail(new PersistentResourceNotAvailableError());
+    }
+    catch (DbUpdateException exception)
+    {
+      _logger.LogError(exception, "Error while adding a NukiCredential (draft) to the database");
+      if (exception.InnerException?.InnerException is NpgsqlException &&
+          exception.InnerException.InnerException.HResult >
+          int.Parse(PostgresErrorCodes.IntegrityConstraintViolation) &&
+          exception.InnerException.InnerException.HResult <
+          int.Parse(PostgresErrorCodes.CheckViolation))
+      {
+        return Result.Fail(new DuplicateEntryError("Nuki credential"));
+      }
+
+      return Result.Fail(new PersistentResourceNotAvailableError());
+    }
+    catch (Exception exception)
+    {
+      _logger.LogError(exception, "Error while adding a NukiCredential (draft) to the database");
+      return Result.Fail(new KerberoError());
+    }
   }
 
   public async Task<Result<NukiCredentialModel>> GetById(int id)
