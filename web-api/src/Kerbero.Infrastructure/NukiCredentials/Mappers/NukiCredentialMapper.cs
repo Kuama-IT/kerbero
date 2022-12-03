7b63e97510340219b380ae7b48d9f362e622b836
Daniele De Matteo <daniele@kuama.net>
2022-12-03 14:13:02 +0100

    feat: implement nuki credential repository (draft credential part)

--- web-api/src/Kerbero.Infrastructure/NukiCredentials/Mappers/NukiCredentialMapper.cs
+++ web-api/src/Kerbero.Infrastructure/NukiCredentials/Mappers/NukiCredentialMapper.cs
@@ -1,4 +1,5 @@
 using Kerbero.Domain.NukiCredentials.Models;
+using Kerbero.Infrastructure.NukiCredentials.Dtos;
 using Kerbero.Infrastructure.NukiCredentials.Entities;
 
 namespace Kerbero.Infrastructure.NukiCredentials.Mappers;
@@ -27,5 +28,41 @@ public static class NukiCredentialMapper
     {
       Token = model.Token,
     };
+  }
+  
+  public static NukiCredentialEntity Map(NukiCredentialDraftModel model)
+  {
+    return new NukiCredentialEntity()
+    {
+      IsRefreshable = true,
+      IsDraft = true,
+      UserId = model.UserId,
+    };
+  }
+
+  public static void Map(NukiCredentialEntity entity, NukiCredentialModel model)
+  {
+    entity.Token = model.Token;
+  }
+
+  public static void Map(NukiCredentialEntity entity, NukiRefreshableCredentialModel model)
+  {
+    entity.Token = model.Token;
+    entity.RefreshToken = model.RefreshToken;
+    entity.CreatedAt = model.CreatedAt;
+    entity.ExpiresIn = model.TokenExpiresIn;
+    entity.IsRefreshable = true;
+    entity.IsDraft = false;
+  }
+
+  public static NukiCredentialDraftModel MapAsDraft(NukiCredentialEntity entity)
+  {
+    return new NukiCredentialDraftModel(UserId: entity.UserId, Id: entity.Id);
+  }
+
+  public static NukiRefreshableCredentialModel Map(NukiOAuthResponseDto entity, DateTime createdAt)
+  {
+    return new NukiRefreshableCredentialModel(Token: entity.Token, Error: entity.Error, RefreshToken: entity.RefreshToken,
+      TokenExpiresIn: entity.TokenExpiresIn, CreatedAt: createdAt);
   }
 }
