93a973f3b0c739110a983d1df097ad88abb7caad
Nicola <nicola.salvadore@gmail.com>
2022-12-06 15:27:50 +0100

    feat: open/close/update/delete smart-lock-key api calls

--- web-app/src/smart-lock-keys/api/smart-lock-key-actions.ts
+++ web-app/src/smart-lock-keys/api/smart-lock-key-actions.ts
@@ -1,11 +1,16 @@
 import { httpClient } from "../../shared/http-client/http-client";
 import {
+  CloseSmartLockWithKeyRequestDto,
   CreateSmartLockKeyRequestDto,
   CreateSmartLockKeyRequestDtoSchema,
+  OpenSmartLockWithKeyRequestDto,
+  OpenSmartLockWithKeyRequestDtoSchema,
   SmartLockKeyListResponseDto,
   SmartLockKeyListResponseDtoSchema,
   SmartLockKeyResponseDto,
   SmartLockKeyResponseDtoSchema,
+  UpdateSmartLockKeyRequestDto,
+  UpdateSmartLockKeyRequestDtoSchema,
 } from "./smart-lock-key.schemas";
 
 export const listSmartLockKeys =
@@ -22,5 +27,48 @@ export const createSmartLockKey = async (
 
   const json = await httpClient.post({ endpoint: "smart-lock-keys", request });
 
+  return SmartLockKeyResponseDtoSchema.parse(json);
+};
+
+export const openSmartLockWithKey = async (
+  request: OpenSmartLockWithKeyRequestDto
+): Promise<void> => {
+  OpenSmartLockWithKeyRequestDtoSchema.parse(request);
+
+  await httpClient.post({
+    endpoint: "smart-lock-keys/open-smart-lock",
+    request,
+  });
+};
+
+export const closeSmartLockWithKey = async (
+  request: CloseSmartLockWithKeyRequestDto
+): Promise<void> => {
+  CreateSmartLockKeyRequestDtoSchema.parse(request);
+
+  await httpClient.post({
+    endpoint: "smart-lock-keys/close-smart-lock",
+    request,
+  });
+};
+
+export const updateSmartLockKey = async (
+  smartLockKeyId: string,
+  request: UpdateSmartLockKeyRequestDto
+): Promise<SmartLockKeyResponseDto> => {
+  UpdateSmartLockKeyRequestDtoSchema.parse(request);
+  const json = httpClient.put({
+    endpoint: `smart-lock-keys/${smartLockKeyId}`,
+    request: request,
+  });
+  return SmartLockKeyResponseDtoSchema.parse(json);
+};
+
+export const deleteSmartLockKey = async (
+  smartLockKeyId: string
+): Promise<SmartLockKeyResponseDto> => {
+  const json = httpClient.delete({
+    endpoint: `smart-lock-keys/${smartLockKeyId}`,
+  });
   return SmartLockKeyResponseDtoSchema.parse(json);
 };
