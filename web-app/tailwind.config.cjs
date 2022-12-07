8847264cd8e823871529b3bc52640dcb67132a4d
Nicola <nicola.salvadore@gmail.com>
2022-12-07 11:34:49 +0100

    feat: smart-lock list item component

--- web-app/tailwind.config.cjs
+++ web-app/tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
+    borderRadius: {
+      'none': '0',
+      DEFAULT: '0.75em',
+      'large': '1.25em'
+    },
     extend: {},
   },
   plugins: [],
