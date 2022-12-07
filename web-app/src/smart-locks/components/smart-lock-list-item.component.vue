622be9222dd291cc93e2fcf2c001b8d00523dc85
Nicola <nicola.salvadore@gmail.com>
2022-12-07 11:34:49 +0100

    feat: smart-lock list item component

---
+++ web-app/src/smart-locks/components/smart-lock-list-item.component.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import DoorsIcon from "@/shared/icons/doors-icon.component.vue";
+defineProps<{
+  name: string;
+  keysCount: number;
+}>();
+const emit = defineEmits<{
+  (e: "manage"): void;
+}>();
+</script>
+
+<template>
+  <div
+    role="button"
+    @click="emit('manage')"
+    class="bg-white rounded-large h-36 w-64 m-4 p-5 cursor-pointer"
+  >
+    <div class="flex w-full h-1/3 justify-between">
+      <p class="text-sm text-gray-700">{{ name }}</p>
+    </div>
+    <div class="flex justify-between items-center">
+      <p class="text-sm text-gray-500">{{ keysCount }} keys</p>
+      <div class="rounded w-1/3 p-3 bg-purple-300 aspect-square">
+        <DoorsIcon class="stroke-purple-500 stroke-1"></DoorsIcon>
+      </div>
+    </div>
+  </div>
+</template>
