a8c4eb5a3d92f59846d353708cb692c392d29422
Nicola Salvadore <nicola.salvadore@gmail.com>
2022-12-13 12:24:15 +0100

    feat: datetime-widget-component
    feat: datetime-widget-component

---
+++ web-app/src/shared/components/datetime-widget.component.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="m-4 space-y-3">
+    <p class="text-4xl">today</p>
+    <p>{{ formatDate(now) }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const now = new Date();
+
+const formatDate = (date: Date): string => {
+  const format = new Intl.DateTimeFormat("en-US", {
+    weekday: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    year: "2-digit",
+    month: "2-digit",
+    day: "2-digit",
+  })
+    .format(date)
+    .toLowerCase()
+    .split(",");
+  return format[0] + " " + format[1] + " |" + format[2];
+};
+</script>
